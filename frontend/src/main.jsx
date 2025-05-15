373c42968a821a11134bc99dc85c759fcbf6913e
KmNeetuSingh <itisneetusingh@gmail.com>
2025-05-15 13:45:22 +0530

    added in main file

---
+++ frontend/src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import App from './App';
+import './index.css';
+import { ThemeProvider } from './ThemeContext';  
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>
+);
