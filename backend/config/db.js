fc18253746e5a7c86d82b717e3c2828fccca9f08
KmNeetuSingh <itisneetusingh@gmail.com>
2025-05-15 18:09:59 +0530

    replace localDB from cloud DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connect to Db");
+    console.log("Cloud Database Connected");
   } catch (err) {
     console.log(`${err}`);
   }
