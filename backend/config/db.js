eef6b6af85abb0a55140057aa1f5474481f226fc
KmNeetuSingh <itisneetusingh@gmail.com>
2025-07-13 23:10:36 +0530

    feature updated fixed the error

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Cloud Database Connected");
+    console.log("Db Connected Successfully");
   } catch (err) {
     console.log(`${err}`);
   }
